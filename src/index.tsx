7953a9bf9d04df146567dbc86dc40238070d2a33
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-10-26 12:07:26 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { App } from './components/App';
 import './i18n';
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Suspense fallback={null}></Suspense>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
